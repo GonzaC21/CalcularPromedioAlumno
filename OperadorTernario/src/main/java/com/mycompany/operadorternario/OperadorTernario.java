aec1ff04ca4860fd9ff7f083f6f1b584ec7280d7
Gonzalo <gonzalocallejas4@gmail.com>
2024-08-28 19:26:17 -0300

    update 1
    update 1

---
+++ OperadorTernario/src/main/java/com/mycompany/operadorternario/OperadorTernario.java
@@ -0,0 +1,28 @@
+
+
+package com.mycompany.operadorternario;
+
+import java.util.Scanner;
+
+
+public class OperadorTernario {
+
+    public static void main(String[] args) {
+        
+      double promedio;
+      String condicionFinal;
+      
+      Scanner teclado = new Scanner(System.in);
+      
+        System.out.println("Ingrese el promedio general del alumno");
+        
+        promedio = teclado.nextDouble();
+        
+        
+        condicionFinal = promedio >=6 ? "Aprobado" : "Desaprobado";
+        System.out.println("La condicion final del alumno es:" + condicionFinal);
+        
+        
+        
+    }
+}
